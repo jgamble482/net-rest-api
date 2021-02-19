d6cca0a89fda3e7053e4194167e0a7484f7e3128
Jordan Gamble <jgamble482@gmail.com>
2021-02-19 15:57:49 -0500

    Added exception handling middleware

--- api/Controllers/ErrorController.cs
+++ api/Controllers/ErrorController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using api.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +13,44 @@ namespace api.Controllers
     [ApiController]
     public class ErrorController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public ErrorController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public IActionResult GetSecret()
+        {
+            return Ok("secret");
+        }
+
+        [HttpGet("not-found")]
+        public IActionResult GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+
+            if (thing == null) return NotFound();
+
+            return Ok(thing);
+        }
+
+        [HttpGet("server-error")]
+        public IActionResult GetServerError()
+        {
+            var thing = _context.Users.Find(-1);
+
+            var thingToReturn = thing.ToString();
+
+            return Ok(thingToReturn);
+        }
+
+        [HttpGet("bad-request")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest("Bad Request");
+        }
     }
 }
