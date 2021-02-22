89248b0c708dea9439ece1dbaf9077c7bd7256a2
Jordan Gamble <jgamble482@gmail.com>
2021-02-22 16:39:32 -0500

    Added props to AppUser, created photo class

---
+++ api/Entities/Photo.cs
@@ -0,0 +1,15 @@
+﻿namespace api.Entities
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+
+        public bool IsMain { get; set; }
+
+        public string Url { get; set; }
+
+        public string PublicId { get; set; }
+
+
+    }
+}
