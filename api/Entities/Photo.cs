767a7b051c27dc3578d748c1f0bfb88ef05245c2
Jordan Gamble <jgamble482@gmail.com>
2021-02-22 17:20:28 -0500

    Added migration for the extended user entitiy

--- api/Entities/Photo.cs
+++ api/Entities/Photo.cs
@@ -1,5 +1,8 @@
-﻿namespace api.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Entities
 {
+    [Table("Photos")]
     public class Photo
     {
         public int Id { get; set; }
@@ -9,6 +12,10 @@
         public string Url { get; set; }
 
         public string PublicId { get; set; }
+
+        public AppUser AppUser { get; set; }
+
+        public int AppUserId { get; set; }
 
 
     }
