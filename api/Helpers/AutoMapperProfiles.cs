8cbb9873de3337b65e2947f537578c4fc01bb2c8
Jordan Gamble <jgamble482@gmail.com>
2021-08-12 17:12:00 -0400

    Fixed UTC date formats, optimized query to get user's gender

--- api/Helpers/AutoMapperProfiles.cs
+++ api/Helpers/AutoMapperProfiles.cs
@@ -22,7 +22,6 @@ namespace api.Helpers
             CreateMap<Message, MessageDTO>()
                 .ForMember(dest => dest.SenderPhotoUrl, opt => opt.MapFrom(src => src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.MapFrom(src => src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url));
-            CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
 
         }
     }
